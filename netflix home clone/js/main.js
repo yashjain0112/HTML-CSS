78dc9aca9f4c7dd736591b2d7ae06d98b044061c
Yash Jain <56643041+yashjain0112@users.noreply.github.com>
2020-05-06 19:15:58 +0530

    Add files via upload

---
+++ netflix home clone/js/main.js
@@ -0,0 +1,27 @@
+const tabItems = document.querySelectorAll('.tab-item');
+const tabContentItems = document.querySelectorAll('.tab-content-item');
+
+// Select Tab Content item
+function selectItem(e) {
+    removeBorder();
+    removeShow();
+    // Add border to current tab
+    this.classList.add('tab-border');
+    // Grab content item from the dom 
+    const tabContentItem = document.querySelector(`#${this.id}-content`);
+    // add show class
+    tabContentItem.classList.add('show');
+}
+
+function removeBorder () {
+    tabItems.forEach(item => item.classList.remove('tab-border'));
+}
+
+function removeShow () {
+    tabContentItems.forEach(item => item.classList.remove('show'));
+}
+
+// Listen for each click
+tabItems.forEach(item => {
+    item.addEventListener('click', selectItem);
+});
